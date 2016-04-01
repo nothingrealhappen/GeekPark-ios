8f1009855dd757960fadaed850bc46c3279834a0
ScorpiusZ <520095417@qq.com>
2016-04-01 11:49:32 +0800

    completion launchView animation

---
+++ GeekPark/LaunchView.swift
@@ -0,0 +1,51 @@
+//
+//  LaunchView.swift
+//  GeekPark
+//
+//  Created by ScorpiusZ on 16/4/1.
+//  Copyright © 2016年 GeekPark. All rights reserved.
+//
+
+import UIKit
+
+class LaunchView: UIView{
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  static func showAtWindow(image: String? = nil) {
+    
+    let launchView = NSBundle.mainBundle().loadNibNamed("LaunchView", owner: self, options: nil).first as! LaunchView
+    
+    let window = UIApplication.sharedApplication().keyWindow
+    launchView.frame = window!.bounds
+    window?.addSubview(launchView)
+    
+    if let imageUrl = image{
+      launchView.imageView.kf_setImageWithURL(NSURL(string: imageUrl)!)
+    }
+    
+    
+    UIView.animateWithDuration(2.4, animations: {
+      let scaleTransform: CGAffineTransform  = CGAffineTransformMakeScale(20, 20);
+      launchView.imageView.transform = scaleTransform
+      launchView.imageView.alpha = 0
+      }, completion: { completion in
+        if completion {
+          launchView.removeFromSuperview()
+        }
+      }
+    )
+    
+  }
+  
+
+  
+  deinit {
+    //        print("SecondLaunchView--deinit")
+  }
+
+}
