604e72c317cad0ab9e4f2deb70b51661cda5c760
zhukun <zhukun6150909@outlook.com>
2016-04-11 21:33:30 +0800

    finish activity comment

--- GeekPark/ActivityDetailViewController.swift
+++ GeekPark/ActivityDetailViewController.swift
@@ -29,7 +29,7 @@ class ActivityDetailViewController: FullScreenViewController {
   func addTopNavigationbar(){
     //添加头部的navigationBar
     topNavigationBar = NSBundle.mainBundle().loadNibNamed("CTTopNavigationBar", owner: self, options: nil).first as! CTTopNavigationBar
-    let naviHeight = navigationController!.navigationBar.frame.height + 10
+    let naviHeight = navigationController?.navigationBar.frame.height ?? 0 + 10
     topNavigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: naviHeight)
     topNavigationBar.topNavigationBarDelegate = self
     topNavigationBar.style = .like
@@ -44,8 +44,6 @@ class ActivityDetailViewController: FullScreenViewController {
     
     Activity.member(activity_id){ activity in
       self.activity = activity
-      print("#####")
-      print(activity.banner)
       self.headerDatas = [activity.banner ?? "", "活动简介", "活动日程", "\(activity.audiences?.count ?? 0)人已报名", "\(activity.comments?.count ?? 0)条评论"]
       self.datas = [activity.infoDictionary(), [activity.introduction ?? ""], [activity.speeches ?? []], [activity.audiences ?? []], [activity.comments ?? []]]
     }
--- GeekPark/Comment.swift
+++ GeekPark/Comment.swift
@@ -15,14 +15,67 @@ class Comment: BaseModel{
   var depth: Int = 0
   var parent_id = ""
   var user: User?
+  var ancestry = ""
+  static let names = ["R2-D2", "C-3PO", "BB-8"]
   
   static func list(commentType: String, commentable_id: String, callback: ([Comment]) -> Void){
     doRequest(router: "comments", api_root: main_api_root, params: ["commentable_type": commentType, "commentable_id": commentable_id]){ status, data in
       if status == .SUCCESS{
-        let comments = [Comment](json: data!.rawString())
+        var comments = [Comment](json: data!.rawString())
+        comments = Comment.sort(comments)
+        comments = Comment.handleAnonymous(comments)
+        comments = Comment.handleCommentInherit(comments)
         callback(comments)
       } else {
       }
     }
   }
+  
+  static func sort(comments: [Comment]) -> [Comment]{
+    return comments.sort(){ comment1, comment2 in
+      return comment1.ancestry < comment2.ancestry
+    }
+  }
+  
+  static func handleAnonymous(comments: [Comment]) -> [Comment]{
+    return comments.map{ comment in
+      if comment.user == nil{
+        comment.user = User(json: "{\"username\": \"\(getRandomName())\"}")
+        print(comment.user)
+      }
+      return comment
+    }
+  }
+  
+  static func handleCommentInherit(comments: [Comment]) -> [Comment]{
+    return comments.map{ comment in
+      if comment.parent_id != ""{
+        if let parent = find_by_id(comment.parent_id, comments: comments){
+          comment.body = "回复 \(parent.user?.username ?? ""): " + (comment.body ?? "")
+        }
+      }
+      return comment
+    }
+  }
+  
+  static func find_by_id(id: String, comments: [Comment]) -> Comment?{
+    for comment in comments {
+      if comment.id == id{ return comment }
+    }
+    return nil
+  }
+  
+  static func getRandom() -> UInt32{
+    return arc4random()
+  }
+  
+  static func getRandomAvator() -> String{
+    return "\(getRandom()%8)"
+  }
+  
+  static func getRandomName() -> String{
+    let index = Int(getRandom()%3)
+    return names[index]
+  }
+
 }
